05062d9fc228d36aa384d1427c76b5cde403cc42
John Maloney <jhmaloney@gmail.com>
2024-12-12 07:46:30 -0500

    When re-using an existing file entry, don't close and re-open, just seek to start

--- vm/filePrims.cpp
+++ vm/filePrims.cpp
@@ -96,9 +96,7 @@ static OBJ primOpen(int argCount, OBJ *args) {
 	if (!fileName[0]) return falseObj;
 
 	int i = entryFor(fileName);
-	if (i >= 0) { // found an existing entry; close and reopen
-		fileEntry[i].file.close();
-		fileEntry[i].file = myFS.open(fileName, "a+");
+	if (i >= 0) { // use the existing entry
 		fileEntry[i].file.seek(0, SeekSet); // read from start of file
 		return falseObj;
 	}
