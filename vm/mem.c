8bdb3cf41e2ac20eec7f1e59ba190b2d9550d6d2
John Maloney <jhmaloney@gmail.com>
2025-05-04 08:36:54 -0400

    Decrease objectstore slight for micro:bit v1 and Calliope v1

--- vm/mem.c
+++ vm/mem.c
@@ -38,7 +38,7 @@
 // update (forward) references of objects that move during compaction and object resizing.
 
 #if defined(NRF51)
-	#define OBJSTORE_BYTES 1200
+	#define OBJSTORE_BYTES 1120
 #elif defined(ARDUINO_BBC_MICROBIT_V2) || defined(ARDUINO_CALLIOPE_MINI_V3)
 	#define OBJSTORE_BYTES 46000
 #elif defined(ARDUINO_NRF52_PRIMO)
