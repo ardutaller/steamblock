0e7d04f108d36466f9b65be771d45d3c07495174
John Maloney <jhmaloney@gmail.com>
2025-05-21 08:06:38 -0400

    Add XRP vendor ID

--- chromeApp/webapp/gpSupport.js
+++ chromeApp/webapp/gpSupport.js
@@ -943,6 +943,7 @@ async function webSerialConnect() {
 		{ usbVendorId: 0x2E8A},		// Raspberry Pi Pico RP2040
 		{ usbVendorId: 0x303a},		// Espressif USB JTAG/serial debug unit
 		{ usbVendorId: 0x0483},		// STMicroelectronics
+		{ usbVendorId: 0x1B4F},		// XRP
 	];
 	webSerialDisconnect();
 	GP_webSerialPort = await navigator.serial.requestPort({filters: vendorIDs}).catch((e) => { console.log(e); });
