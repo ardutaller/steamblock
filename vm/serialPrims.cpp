4c6fc7bd59042aca8663f370d127cdbc9566cbef
John Maloney <jhmaloney@gmail.com>
2025-08-05 08:17:41 -0400

    Fix serial on Xiao ESP32-C3

--- vm/serialPrims.cpp
+++ vm/serialPrims.cpp
@@ -223,7 +223,7 @@ static void serialOpen(int baudRate) {
 		SERIAL_PORT.begin(baudRate, SERIAL_8N1, rxPin, txPin);
 	#elif defined(COCUBE)
 		SERIAL_PORT.begin(baudRate, SERIAL_8N1, 22, 21);
-	#elif defined(C3_SUPERMINI)
+	#elif defined(C3_SUPERMINI) || defined(ESP32_C3)
 		SERIAL_PORT.begin(baudRate, SERIAL_8N1, 20, 21);
 	#elif defined(M5CORE2)
 		SERIAL_PORT.begin(baudRate, SERIAL_8N1, 32, 33);
