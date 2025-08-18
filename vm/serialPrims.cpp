65583f967b5cd03bc14d8dc8f4c75bee9139acb7
John Maloney <jhmaloney@gmail.com>
2025-08-18 07:07:54 -0400

    Fix serial pins for generic ESP32-C3

--- vm/serialPrims.cpp
+++ vm/serialPrims.cpp
@@ -251,6 +251,8 @@ static void serialOpen(int baudRate) {
 		SERIAL_PORT.begin(baudRate);
 		delayMicroseconds(5); // wait for garbage byte when first opening the serial port after a reset (seen at 115200 baud)
 		SERIAL_PORT.begin(baudRate); // reset to discard garbage byte
+	#elif defined(ESP32_C3)
+		SERIAL_PORT.begin(baudRate, SERIAL_8N1, 10, 9);
 	#elif defined(ESP32)
 		// all ESP32 boards that do not have cases above
 		SERIAL_PORT.begin(baudRate, SERIAL_8N1, 16, 17);
