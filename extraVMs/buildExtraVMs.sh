f7c55bda2215b08d17277bb4d68b67d179b18237
John Maloney <jhmaloney@gmail.com>
2025-09-06 08:01:13 -0400

    Remove makerportV1 from extra VM's (obsolete)

--- extraVMs/buildExtraVMs.sh
+++ extraVMs/buildExtraVMs.sh
@@ -25,8 +25,6 @@ pio run -e mbits
 cp .pio/build/mbits/firmware.bin extraVMs/vm_mbits.bin
 pio run -e pico-xrp
 cp .pio/build/pico-xrp/firmware.uf2 extraVMs/vm_pico_xrp.uf2
-pio run -e makerportV1
-python precompiled/uf2conv.py -c .pio/build/makerportV1/firmware.bin -o extraVMs/vm_makerport_v1.uf2
 pio run -e gizmo-mechatronics
 cp .pio/build/gizmo-mechatronics/firmware.uf2 extraVMs/vm_gizmo_mechatronics.uf2
 pio run -e wukong2040-w
