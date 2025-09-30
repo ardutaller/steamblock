3bbacf85b86f733e68132389c0288ff65ec4b1de
John Maloney <jhmaloney@gmail.com>
2025-09-29 20:05:51 -0400

    Preliminary support for selecting USB device (works) Attempt at using same mechanism to select a BLE device (does not work)

--- electron/src/index.js
+++ electron/src/index.js
@@ -1,9 +1,24 @@
-const { app, BrowserWindow } = require('electron');
+const { app, ipcMain, BrowserWindow, Menu, MenuItem } = require('electron');
 const path = require('node:path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
 	app.quit();
+}
+
+async function showConnectionMenu(portList, callback) {
+	function usbSelected(menuItem, window, event) {
+		console.log("selected", menuItem.label, menuItem.id);
+		callback(menuItem.id);
+	}
+
+console.log("portlist", portList);
+	const myMenu = new Menu();
+	for (var i = 0; i < portList.length; i++) {
+		var fullName = portList[i].displayName + ' (' + portList[i].portName + ')';
+		myMenu.append(new MenuItem({label: fullName, click: usbSelected, id: portList[i].portId}));
+	}
+	myMenu.popup();
 }
 
 const createWindow = () => {
@@ -27,21 +42,29 @@ const createWindow = () => {
 	mainWindow.webContents.session.setDevicePermissionHandler((details) => {
 		return (details.deviceType === 'serial');
 	});
-	
+
+	// Force-quit electron when window is closed (needed on MacOS).
+	mainWindow.on('close', (e) => {
+		app.exit();
+	});
 
 	mainWindow.webContents.session.on(
 		'select-serial-port',
 		(event, portList, webContents, callback) => {
 			event.preventDefault();
-			console.log(portList);
-			const selectedPort = portList.find((device) => {
-				return device;
-			})
-			if (!selectedPort) {
-				callback('');
-			} else {
-				callback(selectedPort.portId);
-			}
+			showConnectionMenu(portList, callback);
+	});
+
+	let selectBluetoothCallback;
+
+	mainWindow.webContents.on('select-bluetooth-device', (event, deviceList, callback) => {
+		event.preventDefault();
+		selectBluetoothCallback = callback;
+		showConnectionMenu(deviceList, callback);
+	});
+
+	ipcMain.on('cancel-bluetooth-request', (event) => {
+		selectBluetoothCallback('');
 	});
 
 	// open devTools when in dev mode
