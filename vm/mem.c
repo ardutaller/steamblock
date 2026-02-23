56ec20d0230d227035e94d458eb013183d9789aa
John Maloney <jhmaloney@gmail.com>
2026-02-23 09:57:52 -0500

    Adjust memory so the WiFi is relaible on the C3 (adding SecureHTTPClient broke WiFI; apparently it consumes memory even when now used).

--- vm/mem.c
+++ vm/mem.c
@@ -49,7 +49,9 @@
 	#define OBJSTORE_BYTES 11000
 #elif defined(HAS_CAMERA)
 	#define OBJSTORE_BYTES 230000 // will be allocated from PSRAM
-#elif defined(ESP32_S2) || defined(ESP32_S3) || defined(ESP32_C3)
+#elif defined(ESP32_S2) || defined(ESP32_C3)
+	#define OBJSTORE_BYTES 32000 // https test: 80000 fails 50000 fails on https 35000 works
+#elif defined(ESP32_S3)
 	#define OBJSTORE_BYTES 100000
 #elif defined(ARDUINO_ARCH_ESP32)
 	// object store is allocated from heap on ESP32
