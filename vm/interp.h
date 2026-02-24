79d527fc41effc580772190837f04c406091212c
John Maloney <jhmaloney@gmail.com>
2026-02-24 15:09:45 -0500

    Add code to convert from Unicode to CP437 characters in the Adafruit GFX font (thanks, Ilya Zverev!)

--- vm/interp.h
+++ vm/interp.h
@@ -275,10 +275,12 @@ void runTasksUntilDone(void);
 void interpTests1(void);
 void taskTest(void);
 
-// UTF-8 Utility
+// UTF-8 Utilities (from dataPrims.c)
 
 int countUTF8(char *s);
 OBJ charAt(OBJ stringObj, int i);
+int unicodeCodePoint(char *s);
+char *nextUTF8(char *s);
 
 // Platform Specific Operations
 
