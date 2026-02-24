469b811a0eb3624c765c10954567a5aab186cef2
John Maloney <jhmaloney@gmail.com>
2026-02-23 20:43:34 -0500

    Fix: Disable External Display support on SAMD21 boards. Although it is supposed to work it crashes the board and prevents the USB port from appearing.

--- vm/tftPrims.cpp
+++ vm/tftPrims.cpp
@@ -12,7 +12,8 @@
 #include "interp.h"
 #include <inttypes.h>
 
-#if defined(ARDUINO_WEACT) || defined(NRF51) || defined(__ZEPHYR__) || defined(DUELink)
+#if defined(ARDUINO_WEACT) || defined(NRF51)  || defined(ARDUINO_ARCH_SAMD) || \
+	defined(__ZEPHYR__) || defined(DUELink)
 
 // TFT primitives are not supported
 #define NO_EXTERNAL_DISPLAY_PRIMS
