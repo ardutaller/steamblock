c60a540e1c43227f9511ecbaa766cc054d0f9bcb
John Maloney <jhmaloney@gmail.com>
2026-04-29 15:16:57 -0400

    Tweaks

--- snapBlocks/buildFileSystem.sh
+++ snapBlocks/buildFileSystem.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -rf Libraries
-cp -r ../../../gp/Libraries .
+cp -r ../gp/Libraries .
 find Libraries -name ".DS_Store" -type f -delete
 
 rm -rf Examples
-cp -r ../../../gp/Examples .
+cp -r ../gp/Examples .
 find Examples -name ".DS_Store" -type f -delete
 
 rm files.txt
