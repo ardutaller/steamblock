3722c07a0f00e412720cbfe261c4959293ad5d28
John Maloney <jhmaloney@gmail.com>
2026-05-30 09:25:28 -0400

    Springbot: Play a two-note beep when the double-tap is sensed

--- vm/vm.ino
+++ vm/vm.ino
@@ -11,14 +11,30 @@
 int resetDoubleTap = false;
 
 #if defined(SPRINGBOT)
+	#define BUZZER_PIN 33
 	#define RED_LED_PIN 40
 	#define DOUBLE_TAP_PIN 42
+
+	void springbotBeep(int halfPeriod) {
+		pinMode(BUZZER_PIN, OUTPUT);
+		for (int i = 0; i < 80; i++) {
+			digitalWrite(BUZZER_PIN, HIGH);
+			delayMicroseconds(halfPeriod);
+			digitalWrite(BUZZER_PIN, LOW);
+			delayMicroseconds(halfPeriod);
+		}
+		pinMode(BUZZER_PIN, INPUT);
+	}
 
 	void checkForDoubleTap() {
 		// read the remembered state as early as possible
 		pinMode(DOUBLE_TAP_PIN, INPUT);
 		resetDoubleTap = digitalRead(DOUBLE_TAP_PIN);
-		if (resetDoubleTap) return;
+		if (resetDoubleTap) {
+			springbotBeep(642); // G
+			springbotBeep(764); // E
+			return;
+		}
 
 		// arm DOUBLE_TAP_PIN for the next reset
 		pinMode(DOUBLE_TAP_PIN, OUTPUT);
@@ -34,8 +50,8 @@ int resetDoubleTap = false;
 		// If it is high, then the reset button was pressed
 		// during the double-tap delay above.
 
-		pinMode(DOUBLE_TAP_PIN, INPUT); // release DOUBLE_TAP_PIN
 		pinMode(RED_LED_PIN, INPUT); // turn off red LED
+		pinMode(DOUBLE_TAP_PIN, INPUT); // release DOUBLE_TAP_PIN
 	}
 
 #endif
